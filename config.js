fb2718d04bb1875b52cac058dee41ea2030c607e
jonmoon74 <jon.moon@outlook.com>
2019-01-04 19:55:31 +0000

    Initial commit

---
+++ config.js
@@ -0,0 +1,4 @@
+const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/2c63fed9-e7ce-4df7-8a33-962ab741944f/token";
+const instanceLocator = "v1:us1:2c63fed9-e7ce-4df7-8a33-962ab741944f";
+
+export { tokenUrl, instanceLocator }
